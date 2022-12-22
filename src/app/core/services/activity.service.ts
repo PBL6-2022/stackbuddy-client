7d5f4ee4d1d77fbd75393c86d0115f9bf9fdfba9
Thinh Le <roshan1234.lol@gmail.com>
2022-12-22 20:21:44 +0700

    Init firebase

--- src/app/core/services/activity.service.ts
+++ src/app/core/services/activity.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { map } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { HttpService } from './http.service';
+import { PrimeIcons } from 'primeng/api';
 
 @Injectable({ providedIn: 'root' })
 export class ActivityLogService {
@@ -11,9 +12,44 @@ export class ActivityLogService {
         private httpService: HttpService,
     ) {}
 
+    
+    Kind = {
+        Login: 'login',
+        Bookmark: 'bookmark',
+        Search: 'search',   
+    };
+
+    StatusMapper = {
+        [this.Kind.Login]: 'You logged in',
+        [this.Kind.Bookmark]: () => {
+            return `You bookmark this question: ${'h'}`;
+        },
+    };
+
+    IconMapper = {
+        [this.Kind.Login]: PrimeIcons.SIGN_IN,
+        [this.Kind.Bookmark]: PrimeIcons.BOOKMARK,
+    }
+
+    buildFromRawData(data: any) {
+        const wrapUpData = data.map((activityData: any) => {
+            const { activity } = activityData;
+            return {
+                status: this.StatusMapper[activity.kind],
+                date: (new Date(activityData.createdAt)).toLocaleString(),
+                icon: this.IconMapper[activity.kind],
+                data: activityData.data,
+            };
+        });
+
+        console.log(wrapUpData);
+
+        return wrapUpData;
+    }
+
     getLogs() {
-        const getDoctorsUrl = `${this.apiGatewayUrl}/api/v1/doctors`;
-        return this.httpService.get({ url: getDoctorsUrl })
+        const getUserLogsUrl = `${this.apiGatewayUrl}/api/v1/activities`;
+        return this.httpService.get({ url: getUserLogsUrl })
             .pipe(map(
                 response => {
                     const dataResponse = response as any;
--- src/app/modules/home/pages/history-page/history-page.component.ts
+++ src/app/modules/home/pages/history-page/history-page.component.ts
@@ -4,6 +4,7 @@ import { MessageService, PrimeIcons } from 'primeng/api';
 import { ActivityLogService } from 'src/app/core/services/activity.service';
 import ActivityLog from 'src/app/core/models/activity';
 import { Subscription } from 'rxjs';
+import { Severity } from 'src/app/core/models/severity';
 
 @Component({
   selector: 'app-history-page',
@@ -13,7 +14,7 @@ import { Subscription } from 'rxjs';
 export class HistoryPageComponent implements OnInit {
 
   events1!: any[];
-
+  activities: any;
   eventLogger: Array<ActivityLog> = [];
   getActivityLogSubscription!: Subscription;
 
@@ -45,11 +46,35 @@ export class HistoryPageComponent implements OnInit {
         icon: PrimeIcons.SEARCH,
       },
     ];
+
+    this.getActivityLog();
   }
 
   getActivityLog() {
+    const onNextHandler: (data: any) => void = (data) => {
+      console.log(data);
+      this.activities = this.activityLogService.buildFromRawData(data);
+      this.messageService.add({
+        severity: Severity.Success,
+        summary: 'Get log success',
+      });
+      console.log({ activities: this.activities })
+      this.getActivityLogSubscription.unsubscribe();
+    };
+
+    const onErrorHandler: (data: any) => void = (data) => {
+      this.messageService.add({
+        severity: Severity.Error,
+        summary: 'Cannot get activity logs',
+      });
+      this.getActivityLogSubscription.unsubscribe();
+    }
+
     this.getActivityLogSubscription = this.activityLogService
       .getLogs()
-      .subscribe();
+      .subscribe({
+        next: (data) => onNextHandler(data),
+        error: (error) => onErrorHandler(error),
+      });
   }
 }
